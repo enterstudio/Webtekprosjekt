119c0dc97a33540defbd59c6698a5d6b2f28c2cc
simeonlg <sisimeon@hotmail.com>
2014-11-20 13:20:39 +0100

    Added project.html
    no description

---
+++ welcome.js
@@ -0,0 +1,25 @@
+var today = new Date();
+var hours = today.getHours();
+var godNatt = document.createTextNode("God natt");
+var godMorgen = document.createTextNode("God morgen");
+var godDag = document.createTextNode("God dag");
+var godKveld = document.createTextNode("God kveld");
+
+
+
+var welcome = document.getElementsByClassName("welcome")[0];
+console.log(welcome);
+haha();
+function haha() {
+
+
+	if (hours >= 0 && hours < 6) {
+		welcome.appendChild(godNatt);
+	} if (hours >= 6 && hours < 12) {
+		welcome.appendChild(godMorgen);
+	} if (hours >= 12 && hours < 18) {
+		welcome.appendChild(godDag);
+	} if (hours >= 18) {
+		welcome.appendChild(godKveld);
+	}
+}
