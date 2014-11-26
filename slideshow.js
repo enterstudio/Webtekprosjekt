ac84b914b9a72f420be1a2e81c32c41fd4044c3d
tu55eladd <groneng93@gmail.com>
2014-11-26 11:14:47 +0100

    Slideshow på g

--- slideshow.js
+++ slideshow.js
@@ -15,6 +15,52 @@ var link = document.getElementById("slideLink");
 
 var timerID
 
+var timerStart
+
+var slideStrings = ["Samfundet","Studieteknikker","Finn de billigste butikkene"]
+
+function nextFrame(){
+	if(slideNumber == slideLength){
+		slideNumber = 0
+	}
+	// Draw picture 
+	ctx.drawImage(slides[slideNumber],0,0,1170,580);
+	console.log(slides[slideNumber])
+	// Draw box
+	ctx.fillStyle="rgba(75, 14, 14, 0.7)"
+	ctx.fillRect(0,440,500,100)
+	// Draw text
+	ctx.font="20px Ubuntu";
+	ctx.fillStyle="white";
+	ctx.fillText(slideStrings[slideNumber],20,490)
+	
+   	document.getElementById("slideLink").href = slideLinks[slideNumber]; 
+
+	slideNumber += 1
+}
+function firstFrame(){
+	if(slideNumber == slideLength){
+		slideNumber = 0
+	}
+	// Draw picture 
+	ctx.drawImage(slides[slideNumber],0,0,1170,580);
+	console.log(slides[slideNumber])
+	console.log("first frame")
+	// Draw box
+	ctx.fillStyle="rgba(75, 14, 14, 0.7)"
+	ctx.fillRect(0,440,500,100)
+	// Draw text
+	ctx.font="20px Ubuntu";
+	ctx.fillStyle="white";
+	ctx.fillText(slideStrings[slideNumber],20,490)
+	
+   	document.getElementById("slideLink").href = slideLinks[slideNumber]; 
+
+	slideNumber += 1
+
+	clearInterval(timerStart)
+}
+
 if(ctx != null)
 {
 	var samfSlide = document.getElementById("samfSlide");
@@ -23,37 +69,21 @@ if(ctx != null)
 	slides[0] = samfSlide
 	slides[1] = hovedSlide
 	slides[2] = butikkSlide
-	slideLinks[0] = "uteliv/uteliv.html"
-	slideLinks[1] = "uteliv/samfundet.html"
-	slideLinks[2] = "uteliv/solsiden.html"
+	slideLinks[0] = "uteliv/samfundet.html"
+	slideLinks[1] = "studentliv/studieteknikk.html"
+	slideLinks[2] = "studentliv/matpris.html"
 	ctx.canvas.width  = samfSlide.width;
   	ctx.canvas.height = samfSlide.height;
 
+  	timerStart = window.setInterval(firstFrame,80);
   	timerID = window.setInterval(nextFrame,5000);
-	
-	slideLength = slides.length
 
-	
+	slideLength = slides.length
 }
 else
 {
 	console.log("feil")
 }
 
-function nextFrame(){
-	if(slideNumber == slideLength){
-		slideNumber = 0
-	}
-	ctx.drawImage(slides[slideNumber],0,0,1170,580);
-	ctx.fillStyle="#4B0E0E";
-	ctx.fillRect(0,440,500,100)
-	ctx.font="20px Ubuntu";
-	ctx.fillStyle="white";
-	ctx.fillText("Samfundet",20,480)
-	
-   	document.getElementById("slideLink").href = slideLinks[slideNumber]; 
 
-	slideNumber += 1
-}
 
-nextFrame();
